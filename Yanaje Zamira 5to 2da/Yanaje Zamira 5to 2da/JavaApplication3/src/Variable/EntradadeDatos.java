f08895b3f4ba2fbfb14c94aecc9e34f65df2aa69
Summu10 <jacquiyanaje@gmail.com>
2025-03-18 16:00:21 -0300

    Add files via upload

---
+++ Yanaje Zamira 5to 2da/Yanaje Zamira 5to 2da/JavaApplication3/src/Variable/EntradadeDatos.java
@@ -0,0 +1,16 @@
+
+package Variable;
+import java.util.Scanner;
+
+public class EntradadeDatos {
+ //METODO MAIN
+    public static void main(String[] args) {
+        String nombre;
+        Scanner leer = new Scanner(System.in);
+        
+        System.out.print("Ingresa tu nombre: ");
+        nombre = leer.next();
+        
+        
+    }
+}
---
+++ Yanaje Zamira 5to 2da/Yanaje Zamira 5to 2da/Nuevo_Proyecto/src/Primer_programa/HolaMundo.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Primer_programa;
+
+/**
+ *
+ * @author LABO_B11
+ */
+public class HolaMundo {
+    //psvm + tab
+    public static void main(String[] args) {
+        //sout + tab
+        //char - String char
+        String nombre = "Zami";
+        int edad = 17;
+        //concatenacion
+        System.out.println("Hola "+nombre+ " Tu edad es: "+edad);
+        System.out.print("Tu edad es: "+edad);
+    }
+}
