31649d4989f6d36aafc53d52b5474f1a320ff731
Summu10 <jacquiyanaje@gmail.com>
2025-03-18 14:50:26 -0300

    prueba

---
+++ Yanaje Zamira 5to 2da/Nuevo_Proyecto/src/Primer_programa/calculadora.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Primer_programa;
+
+/**
+ *
+ * @author LABO_B11
+ */
+public class calculadora {
+    public static void main(String[] args) {
+        int x, y;
+        x = 20;
+        y = 30;
+        System.out.println("La suma de las variables es: "+(x + y ));
+        System.out.println("La resta de las variables es: "+ (x - y));
+        System.out.println("La multiplicación de las variables es: "+(x * y));
+        System.out.println("La división de las variables es: "+(x / y));
+    }
+    
+}
